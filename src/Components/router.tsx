b0168e9d283740b6b9b7bad0039ddf28b15e531a
Goran Gelevski <gorangelevskigti@gmail.com>
2024-03-15 13:05:08 +0100

    added music app

---
+++ src/Components/router.tsx
@@ -0,0 +1,20 @@
+import App from "../App";
+import { createBrowserRouter } from "react-router-dom";
+import NavBarComponent from "./NavBarComponent/NavBarComponent";
+
+//  export const router = createBrowserRouter([
+//     {
+//       element: <App/>,
+//       children: [{
+//         path: '/',
+//         element: <NavBarComponent />,
+//       },
+//     {
+//       path: 'artist:id',
+//       element: ,
+//       },
+//       ],
+//       },
+    
+//      ]);
+    
